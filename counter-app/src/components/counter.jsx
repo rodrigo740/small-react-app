d0ca7d2aae41090be635ea437c4267d96bd59ff6
rodrigo740 <rodrigomartins@ua.pt>
2021-03-16 23:38:51 +0000

    Half way done

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,9 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    render() { 
-        return <div><h1>Hello World!</h1><button>Increment</button></div>;
-    }
+  state = {
+    value: this.props.value
+  };
+
+  handeIncrement = product => {
+    console.log(product);
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  render() {
+    return (<div>
+      <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+      <button onClick={() => this.handeIncrement(1)} className="btn btn-secondary btn-sm">Increment</button>
+    </div>);
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes
+  }
+
+  formatCount() {
+    const { value: count } = this.state;
+    return count === 0 ? 'Zero' : count;
+  }
 }
- 
+
 export default Counter;
